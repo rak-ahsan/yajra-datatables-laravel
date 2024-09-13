16c3ee52c1f895e9c60d2ac1755294ab9c2b4e02
Rakib Ahsan <rakibzubayar@gmail.com>
2024-09-14 01:18:55 +0600

    done

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class,'index']);
 Route::resource('products', ProductController::class);
